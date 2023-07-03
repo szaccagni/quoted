362c2f54cca6e61c1dd513babd84c9103cb963b0
szaccagni <sarahzaccagni@gmail.com>
2023-07-02 22:51:17 -0400

    testing

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -33,7 +33,7 @@ export default function SignIn() {
                             type="email"
                             id="email"
                             name="email"
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                             placeholder="Enter your email"
                             required
                         />
@@ -44,7 +44,7 @@ export default function SignIn() {
                             type="password"
                             id="password"
                             name="password"
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                             placeholder="Password"
                             required
                         />
